de3be6f30e8972bbdc24908581f52a9a4fdf2027
Rocca-Francesco <roccafrance98@yahoo.it>
2023-03-27 15:01:46 +0200

    hello + link + bonus

---
+++ resources/views/hello-link.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ciao Link!</title>
+</head>
+<body>
+  <h1>Ciao Zel.. ehm.. Link!!</h1>
+  <a href="/">Torna a casa!</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $alunno = "Francesco Rocca";
+    return view('home', compact ("alunno"));
 });
+
+Route::get('hello-link', function () {
+    $alunno = "Francesco Rocca";
+    return view('hello-link');
+})->name("hello");
